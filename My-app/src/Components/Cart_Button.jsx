2bcb4025d24a07c8a72ee3944d772297fbd6ea53
tommysoeder <148865051+tommysoeder@users.noreply.github.com>
2024-03-07 12:51:37 +0100

    ignore modules

---
+++ My-app/src/Components/Cart_Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Cart_Button = () => {
+  return (
+    <div>
+        <button>Carrito</button>
+    </div>
+  )
+}
+
+export default Cart_Button
---
+++ My-app/src/Components/Title.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Title = () => {
+  return (
+    <div>
+        <h1>Horse Luis Animal Shelter</h1>
+    </div>
+  )
+}
+
+export default Title
