d158396b0f99f63f601c6835e9a4143b6ed3987c
Jeizer-Valarezo <nakellava@hotmail.com>
2024-03-07 15:10:17 +0100

    Resolviendo errores de compatibilidad

--- My-app/src/App.jsx
+++ My-app/src/App.jsx
@@ -3,11 +3,14 @@ import Navbar from "./Pages/Navbar";
 import Slider from "./Pages/Slider";
 import { Card } from "./Pages/Card";
 import Footer from "./Pages/Footer";
+import HomePage from "./Pages/HomePage"
 import Donacion from "./Components/Donacion";
 import Agradecimiento from "./Components/Agradecimiento";
 import AdoptaPerro from "./Components/AdoptaPerro";
 import AdoptaGato from "./Components/AdoptaGato";
 import Carrito from "./Components/Carrito";
+import SobreNosotros from  "./Components/SobreNosotros"
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
@@ -24,7 +27,7 @@ function App() {
       </Routes>
       <Slider />
       <Card />
-      <Footer />
+      
       <Footer />
     </>
   );
