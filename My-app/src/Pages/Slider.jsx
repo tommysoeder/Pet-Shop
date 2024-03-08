1dd2a89b84e9a5e5e13ee56c6e558e8f6db3ad94
Rosa <rdelrioang73@gmail.com>
2024-03-08 18:06:27 +0100

    subida slider

--- My-app/src/Pages/Slider.jsx
+++ My-app/src/Pages/Slider.jsx
@@ -1,7 +1,33 @@
-const Slider = () => {
-  return (
-    <div>Slider</div>
-  )
-}
+import React, { useState, useEffect } from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import lokaso from '../assets/lokaso.jpg';
+import gatorro from '../assets/gatorro.jpg';
+import horseluis from '../assets/horseluis.jpg';
+import style from '../assets/Slider.scss'
 
-export default Slider
+const Slider = () => {
+  const images = [lokaso, gatorro, horseluis];
+  const [currentImage, setCurrentImage] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentImage((prevImage) => (prevImage + 1) % images.length);
+    }, 3000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <div className="slider-wrapper">
+      <div className="slider-container">
+        <TransitionGroup>
+          <CSSTransition key={currentImage} timeout={800} classNames="fade">
+            <img src={images[currentImage]} alt={`Slide ${currentImage + 1}`} />
+          </CSSTransition>
+        </TransitionGroup>
+      </div>
+    </div>
+  );
+};
+
+export default Slider;
