b15503028c36fef476e49c75af565e6d43bf0500
tommysoeder <148865051+tommysoeder@users.noreply.github.com>
2024-03-08 18:33:34 +0100

    añado root e inicio responsive

--- My-app/src/Pages/Navbar.jsx
+++ My-app/src/Pages/Navbar.jsx
@@ -1,12 +1,15 @@
 import Buttons_Navbar from "../Components/Buttons_Navbar"
 import Logo from "../Components/Logo"
+import Nav_Burger from "../Components/Nav_Burger"
 import '../styles/Navbar.css'
+
 
 const Navbar = () => {
   return (
     <div className="Navbar">
         <Logo />
-        <Buttons_Navbar />        
+        <Buttons_Navbar /> 
+        <Nav_Burger />       
     </div>
   )
 }
