a5533b863ab30ba2b1477b8a3760966b219fb6be
MiguelAngelfnc <miguelmb3112@gmail.com>
2024-03-09 16:10:06 +0100

    añadofooter

--- My-app/src/Components/AdoptaPerro.jsx
+++ My-app/src/Components/AdoptaPerro.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const AdoptaPerro = () => {
-  return (
-    <h1>Adopta un Perro</h1>
-  )
+const AdoptaPerro = (/*props*/) => {
+
 }
 
 export default AdoptaPerro
