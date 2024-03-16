38262612d150cd8b702ea9218aff2206baae284b
Rosa <rdelrioang73@gmail.com>
2024-03-16 19:31:18 +0100

    Prueba 1 new slide

--- My-app/src/Pages/Slider.jsx
+++ My-app/src/Pages/Slider.jsx
@@ -1,32 +1,40 @@
-import React, { useState, useEffect } from 'react';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
+
 import lokaso from '../assets/lokaso.jpg';
 import gatorro from '../assets/gatorro.jpg';
 import horseluis from '../assets/horseluis.jpg';
 import '../styles/Slider.scss'
+import { useEffect, useState } from 'react';
 
-const Slider = () => {
+const Carrusel = () => {
   const images = [lokaso, gatorro, horseluis];
-  const [currentImage, setCurrentImage] = useState(0);
+  const textos= [
+    'Nuestra misión es brindar amor, cuidado y un hogar seguro a cada animalito que cruza nuestro camino, porque creemos en la dignidad y el respeto hacia todas las criaturas vivientes.',
+    'Trabajamos incansablemente para promover la adopción responsable y la conciencia sobre el bienestar animal, construyendo puentes de compasión y empatía en nuestra comunidad.',
+    'En nuestra protectora de animales, nos esforzamos por ser la voz de aquellos que no pueden hablar, defendiendo fervientemente la justicia y el derecho de cada ser vivo a una vida plena y feliz.'
+  ];
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      setCurrentImage((prevImage) => (prevImage + 1) % images.length);
-    }, 6000);
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 6000); // Cambia cada 6 segundos
 
     return () => clearInterval(intervalId);
   }, []);
 
   return (
     <div className="slider-wrapper">
-      <div className="slider-container">
-        <TransitionGroup>
-          <CSSTransition key={currentImage} timeout={800} classNames="fade">
-            <img src={images[currentImage]} alt={`Slide ${currentImage + 1}`} />
-          </CSSTransition>
-        </TransitionGroup>
+
+       <div className='contenido'>
+
+       {images.map((image, index) => (
+          <div key={index} className={index === currentIndex ? 'slide active' : 'slide'}>
+            <img src={image} alt={`Slide ${index + 1}`} />
+            <div className="text-overlay">{textos[index]}</div>
+          </div>
+        ))}
+          </div>
       </div>
-    </div>
   );
 };
 
